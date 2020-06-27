cdd3cda0e30eeef4863eee01efd8a53160730859
Mike Williams <mikewilliams@squareup.com>
2020-06-27 15:54:22 +1000

    Start making it look more like a plugin.

--- fish/config/conf.d/code-home.fish
+++ fish/config/conf.d/code-home.fish
@@ -1,4 +1,5 @@
 alias ch=code-home
 
 set -q CDPATH; or set CDPATH .
-set -gx CDPATH $CDPATH ~/Code/github.com/mdub ~/Code/github.com ~/Code
+set -gx CDPATH $CDPATH ~/Code/github.com/mdub ~/Code/github.com 
+set -gx CDPATH $CDPATH ~/Code
